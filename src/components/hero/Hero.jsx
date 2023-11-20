069fdbd2a423d2d991695c9784bc152601c4686d
Bishnu Basudev Patra <141321371+Bishnu18@users.noreply.github.com>
2023-11-20 11:19:22 +0530

    Update Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-function Hero() {
+function HeroSection() {
   return (
-    <div>Hero</div>
+    <div>
+         <img src="https://static.vecteezy.com/system/resources/previews/004/299/835/original/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-free-vector.jpg" alt="" />
+        {/* <div className="absolute inset-0 flex items-center text-center p-4 justify-center">
+        <div className="bg-black bg-opacity-50 p-4 rounded-lg">
+          <h1 className="text-white  text-2xl font-bold">Your Text Here</h1>
+          <p className="text-white">Additional Text</p>
+          <button className="bg-transparent hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border text-white border-gray-300 hover:border-transparent rounded">
+      Transparent Button
+    </button>
+        </div>
+      </div> */}
+    </div>
   )
 }
 
-export default Hero
+export default HeroSection
+{/* <div className="absolute inset-0 flex items-center text-center p-4 justify-center"> */}
