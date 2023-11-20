a5e1e38029ca917e36492094901200a47c67a28b
Bishnu Basudev Patra <141321371+Bishnu18@users.noreply.github.com>
2023-11-20 12:25:06 +0530

    Update footer.jsx

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,52 +1,100 @@
 import React from 'react'
 
-function Filter() {
-  return (
-    <div>
-                <div className=' container mx-auto px-4 mt-5 '>
-                <div className="p-5 rounded-lg bg-gray-100 drop-shadow-xl border border-gray-200">
-                    <div className="relative">
-                        <div className="absolute flex items-center ml-2 h-full">
-                            <svg className="w-4 h-4 fill-current text-primary-gray-dark" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M15.8898 15.0493L11.8588 11.0182C11.7869 10.9463 11.6932 10.9088 11.5932 10.9088H11.2713C12.3431 9.74952 12.9994 8.20272 12.9994 6.49968C12.9994 2.90923 10.0901 0 6.49968 0C2.90923 0 0 2.90923 0 6.49968C0 10.0901 2.90923 12.9994 6.49968 12.9994C8.20272 12.9994 9.74952 12.3431 10.9088 11.2744V11.5932C10.9088 11.6932 10.9495 11.7869 11.0182 11.8588L15.0493 15.8898C15.1961 16.0367 15.4336 16.0367 15.5805 15.8898L15.8898 15.5805C16.0367 15.4336 16.0367 15.1961 15.8898 15.0493ZM6.49968 11.9994C3.45921 11.9994 0.999951 9.54016 0.999951 6.49968C0.999951 3.45921 3.45921 0.999951 6.49968 0.999951C9.54016 0.999951 11.9994 3.45921 11.9994 6.49968C11.9994 9.54016 9.54016 11.9994 6.49968 11.9994Z" />
-                            </svg>
-                        </div>
-                        <input
-                            type="text"
-                            name="searchkey"
-                            id="searchkey"
-                            placeholder="Search here"
-                            className="px-8 py-3 w-full rounded-md bg-violet-0 border-transparent outline-0 text-sm"  />
-                    </div>
-                    <div className="flex items-center justify-between mt-4">
-                        <p className="font-medium">
-                            Filters
-                        </p>
-                        <button className="px-4 py-2 bg-gray-50hover:bg-gray-200 text-gray-800 text-sm font-medium rounded-md" >
-                            Reset Filter
-                        </button>
-                    </div>
-                    <div>
-                        <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 mt-4">
-                            <select className="px-4 py-3 w-full rounded-md bg-gray-50 border-transparent outline-0 focus:border-gray-500 focus:bg-white focus:ring-0 text-sm" >
-                                <option value="jacket">Jacket</option>
-                                <option value="shirt">shirt</option>
-                                <option value="mobile">mobile</option>
-                                <option value="jacket">Jacket</option>
-                            </select>
-                            <select className="px-4 py-3 w-full rounded-md bg-gray-50 border-transparent outline-0  focus:border-gray-500 focus:bg-white focus:ring-0 text-sm" >
-                                <option value="100">100</option>
-                                <option value="200">200</option>
-                                <option value="300">300</option>
-                                <option value="400">400</option>
-                            </select>
+import { Link } from 'react-router-dom'
 
-                        </div>
+
+
+function Footer() {
+  return (
+    <footer className="text-gray-600 body-font bg-gray-300" >
+            <div className="container px-5 py-24 mx-auto" >
+                <div className="flex flex-wrap md:text-left text-center order-first">
+                    <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+                        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3" >CATEGORIES</h2>
+                        <nav className="list-none mb-10">
+                            <li>
+                            <Link to={'/home'} className="text-gray-600 hover:text-gray-800" >Home</Link>
+                            </li>
+                            <li>
+                            <Link to={'/order'} className="text-gray-600 hover:text-gray-800" >Order</Link>
+                            </li>
+                            <li>
+                            <Link to={'/vocalForLocal'} className="text-gray-600 hover:text-gray-800" >Vocal For Local</Link>
+                            </li>
+                            <li>
+                            <Link to={'/Cart'} className="text-gray-600 hover:text-gray-800" >Cart</Link>
+                            </li>
+                        </nav>
+                    </div>
+                    <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+                        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3 uppercase" >Customer Service</h2>
+                        <nav className="list-none mb-10">
+                            <li>
+                                <Link to={'/returnpolicy'} className="text-gray-600 hover:text-gray-800" >Return Policy</Link>
+                            </li>
+                            <li>
+                                <Link to={'/about'} className="text-gray-600 hover:text-gray-800" >About</Link>
+                            </li>
+                            <li>
+                                <Link to={'/contact'} className="text-gray-600 hover:text-gray-800" >Contact Us</Link>
+                            </li>
+                        </nav>
+                    </div>
+
+                    <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+                        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">Services</h2>
+                        <nav className="list-none mb-10">
+                            <li>
+                                <Link to={'/privacypolicy'} className="text-gray-600 hover:text-gray-800">Privacy</Link>
+                            </li>
+
+                        </nav>
+                    </div>
+                    <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+                        <img src="https://ecommerce-sk.vercel.app/pay.png" alt="" />
                     </div>
                 </div>
+
             </div>
-    </div>
+
+            <div className="bg-gray-200">
+                <div className="container px-5 py-3 mx-auto flex items-center sm:flex-row flex-col">
+                    <Link to={'/'} className='flex'>
+                        <div className="flex ">
+                            <h1 className=' text-2xl font-bold text-black  px-2 py-1 rounded'>E-Bharat</h1>
+                        </div>
+                    </Link>
+                    <p className="text-sm text-gray-500 sm:ml-600 sm:mt-0 mt-4">© 2023 E-bharat —
+                        <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank" >www.ebharat.com</a>
+                    </p>
+                    <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+                        <a className="text-gray-500">
+                            <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-5 h-5" viewBox="0 0 24 24">
+                                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
+                            </svg>
+                        </a>
+                        <a className="ml-3 text-gray-500">
+                            <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-5 h-5" viewBox="0 0 24 24">
+                                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
+                            </svg>
+                        </a>
+                        <a className="ml-3 text-gray-500">
+                            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-5 h-5" viewBox="0 0 24 24">
+                                <rect width={20} height={20} x={2} y={2} rx={5} ry={5} />
+                                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
+                            </svg>
+                        </a>
+                        <a className="ml-3 text-gray-500">
+                            <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={0} className="w-5 h-5" viewBox="0 0 24 24">
+                                <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z" />
+                                <circle cx={4} cy={4} r={2} stroke="none" />
+                            </svg>
+                        </a>
+                    </span>
+                </div>
+            </div>
+        </footer>
   )
 }
 
-export default Filter
+export default Footer
